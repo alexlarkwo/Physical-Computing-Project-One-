76a0b7eb9ef5e666423fb4d0907410156ef993f6
Alex <52811190+alexlarkwo@users.noreply.github.com>
2019-08-18 21:24:56 +1200

    Add files via upload
    Test code for servo motor

---
+++ sound_sensor_test.ino
@@ -0,0 +1,16 @@
+int soundSensor = 11;
+int motor = ;
+int blueled = ;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (soundSensor, INPUT);
+pinMode (motor,OUTPUT)
+pinMode (blueled, OUPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
